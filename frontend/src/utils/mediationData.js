882196676d4d69a3266fd7e1e985e54033b913b2
Varun Kudva <varunkudva111@gmail.com>
2025-05-30 00:39:17 +0530

    Create mediationData.js

---
+++ frontend/src/utils/mediationData.js
@@ -0,0 +1,48 @@
+export const mediation = [
+    {
+        title: "Creating Mental Health & Wellbeing",
+        author: "Sadhguru",
+        image: 'https://img.youtube.com/vi/HRXrfIvRzNY/maxresdefault.jpg',
+        video: 'https://youtu.be/HRXrfIvRzNY?si=LgPf1y0SXiGoFIHj',
+        preview: 'https://www.youtube.com/embed/HRXrfIvRzNY?autoplay=1&mute=1'
+    },
+
+
+    {
+        title: "5 Tips to Improve your Mental Health",
+        author: "Sadhguru",
+        image: 'https://img.youtube.com/vi/wOGqlVqyvCM/maxresdefault.jpg',
+        video: 'https://youtu.be/wOGqlVqyvCM?si=kXWDuf9Zspy8e2Wl',
+        preview: 'https://www.youtube.com/embed/wOGqlVqyvCM?autoplay=1&mute=1'
+    },
+
+
+    {
+        title: "How to manage your mental health",
+        author: "Leon Taylor",
+        image: 'https://img.youtube.com/vi/rkZl2gsLUp4/maxresdefault.jpg',
+        video: 'https://youtu.be/rkZl2gsLUp4?si=vfB9OeStf90WH-mb',
+        preview: 'https://www.youtube.com/embed/rkZl2gsLUp4?autoplay=1&mute=1'
+    },
+
+
+    {
+        title: "Atomic Habits for Mental Health",
+        author: "Therapy in a Nutshell",
+        image: 'https://img.youtube.com/vi/AOHT-YiOeQA/maxresdefault.jpg',
+        video: 'https://youtu.be/AOHT-YiOeQA?si=Kxh3nbfIAIpnzguA',
+        preview: 'https://www.youtube.com/embed/AOHT-YiOeQA?autoplay=1&mute=1'
+    },
+
+
+    {
+        title: "Daily habits for mental health",
+        author: "The Yoga Institute",
+        image: 'https://img.youtube.com/vi/ki_A3pSj45A/maxresdefault.jpg',
+        video: 'https://youtu.be/ki_A3pSj45A?si=hQ7Y_dPkatNStX96',
+        preview: 'https://www.youtube.com/embed/ki_A3pSj45A?autoplay=1&mute=1'
+    },
+
+
+
+]
