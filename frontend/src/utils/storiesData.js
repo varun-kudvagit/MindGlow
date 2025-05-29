4e2b9c2499c4dde4a804e770050a31b72590fd10
Varun Kudva <varunkudva111@gmail.com>
2025-05-30 00:38:42 +0530

    Create storiesData.js

---
+++ frontend/src/utils/storiesData.js
@@ -0,0 +1,38 @@
+ export const stories = [
+    {
+        title: "Piano Music",
+        music: 'https://res.cloudinary.com/dmdlgpurh/video/upload/v1736842679/relaxing-piano-music-272714_ltng31.mp3'
+    },
+
+    {
+        title: "Sedative",
+        music: 'https://res.cloudinary.com/dmdlgpurh/video/upload/v1736842498/sedative-110241_talkli.mp3'
+    },
+    
+
+    {
+        title: "Evening Glow",
+        music: 'https://res.cloudinary.com/dmdlgpurh/video/upload/v1736842226/evening-glow-soft-piano-music-243818_azsr6g.mp3'
+    },
+    
+
+    {
+        title: "Aura",
+        music: 'https://res.cloudinary.com/dmdlgpurh/video/upload/v1736842751/aura-273351.mp3_hgvv5q.mp3'
+    },
+    
+
+    {
+        title: "Whispering",
+        music: 'https://res.cloudinary.com/dmdlgpurh/video/upload/v1736842895/whispering-vinyl-loops-lofi-beats-281193_ifwcmp.mp3'
+    },
+    
+
+    {
+        title: "Soft Piano Music",
+        music: 'https://res.cloudinary.com/dmdlgpurh/video/upload/v1736842972/soft-piano-music-272453_jvtgv1.mp3'
+    },
+    
+  
+    
+ ]
