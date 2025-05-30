3274d2af6a8dee29c73de31c8b525fb17dc184ae
Varun Kudva <varunkudva111@gmail.com>
2025-05-30 15:45:22 +0530

    Create authService.js

---
+++ frontend/src/features/authService.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+const registerRoute = "https://mental-wellness-backend.vercel.app/user/sign"
+
+
+const loginRoute =    "https://mental-wellness-backend.vercel.app/user/login"
+
+
+
+
+
+// Register user
+const register = async (userData) => {
+  const response = await axios.post(registerRoute, userData)
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
+}
+
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(loginRoute, userData)
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
+}
+
+// Logout user
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+const authService = {
+  register,
+  logout,
+  login,
+}
+
+export default authService
